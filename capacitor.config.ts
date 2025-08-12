2149c7e4e59209e0b7b244529a409fd6da6633af
adsurkasur <adsurkasur.dev@gmail.com>
2025-08-13 00:20:24 +0700

    Disable web contents debugging in Android configuration

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -36,7 +36,7 @@ const config: CapacitorConfig = {
   android: {
     allowMixedContent: true,
     captureInput: true,
-    webContentsDebuggingEnabled: true
+    webContentsDebuggingEnabled: false
   }
 };
 
