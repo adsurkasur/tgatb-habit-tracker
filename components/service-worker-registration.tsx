0f6ffc269b3639143dea884a7dec93da4b652f89
adsurkasur <adsurkasur.dev@gmail.com>
2025-08-15 02:12:40 +0700

    fixed lint error

--- components/service-worker-registration.tsx
+++ components/service-worker-registration.tsx
@@ -40,7 +40,7 @@ export function ServiceWorkerRegistration() {
               }
             });
           })
-          .catch(error => {
+          .catch(_error => {
             // ...existing code...
           });
       }
