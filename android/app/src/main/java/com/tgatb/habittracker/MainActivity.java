3321c6f5fe29d8d2308f0dca0fc7b8e4ce162335
adsurkasur <adsurkasur.dev@gmail.com>
2026-03-28 06:39:08 +0700

    Reapply "chore: disable Jetifier and enable configuration cache in gradle.properties"
    This reverts commit 621e4dd4e71904c29713a241cba7209aaa7fa304.

--- android/app/src/main/java/com/tgatb/habittracker/MainActivity.java
+++ android/app/src/main/java/com/tgatb/habittracker/MainActivity.java
@@ -1,6 +1,5 @@
 package com.tgatb.habittracker;
 
-import android.os.Build;
 import android.os.Bundle;
 import android.graphics.Color;
 import androidx.core.view.WindowCompat;
@@ -17,14 +16,10 @@ public class MainActivity extends BridgeActivity {
         getBridge().getWebView().setBackgroundColor(Color.TRANSPARENT);
 
         // IMPROVED: Proper window insets handling based on fullscreen state
-        if (Build.VERSION.SDK_INT < 35) {
-            if (SystemUiPlugin.isFullscreenEnabled()) {
-                WindowCompat.setDecorFitsSystemWindows(getWindow(), false);
-            } else {
-                WindowCompat.setDecorFitsSystemWindows(getWindow(), true);
-            }
+        if (SystemUiPlugin.isFullscreenEnabled()) {
+            WindowCompat.setDecorFitsSystemWindows(getWindow(), false);
         } else {
-            // Android 15+ is edge-to-edge by default; avoid deprecated API call.
+            WindowCompat.setDecorFitsSystemWindows(getWindow(), true);
         }
         SystemUiPlugin.reapply(this);
     }
